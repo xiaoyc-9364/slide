b1c47f81c2a6566d4050a9b95e4f132aee2cc13e
xyc <739759506@qq.com>
2017-11-16 10:55:57 +0800

    first push

---
+++ js/index.js
@@ -0,0 +1,144 @@
+function FlexSlider(id, opts) {		//包裹元素的id，opts包含一个图片路径数组的对象
+	var wrap = document.getElementById(id);		//获取包裹元素
+	this.oUl = document.createElement('ul');	//创建ul
+	var aImg = this.oUl.getElementsByTagName('img');
+	var str = '';
+	var wrapWidth = wrap.clientWidth;	//获取包裹元素客户区宽度
+	this.timer = null;
+	this.options = extend(arguments.callee.prototype.defaults, opts);	//拷贝对象
+	var len = this.options.arrImg.length;	
+	wrap.style.cssText += 'overflow: hidden;position: relative;';	//设置包裹元素的css
+	this.oUl.className = 'slide_main';		//ul添加class
+	for (var i = 0; i < len; i++) {
+		var imgUrl = this.options.arrLink[i] ? 'https://' + this.options.arrLink[i] : 'javascript:void(0);';
+		str += "<li><a href='" + imgUrl + "'>"+(''+(i+1))+"<img src='" + this.options.arrImg[i] + "'/></a></li>";
+	}
+	this.oUl.innerHTML = str;
+	//设置图片宽度与包裹元素一致
+	for (var j = 0; j < len; j++) {
+		aImg[j].style.width = wrapWidth + 'px';
+	}
+	//将创建的ul添加到包裹元素中
+	wrap.appendChild(this.oUl);	
+}
+FlexSlider.prototype = {
+	concturctor: FlexSlider,
+	defaults: {
+		// type: 'fade',	//轮播模式 淡入淡出-fade，滑动模式-slide
+		timeout: 2000,	//轮播速度
+		average: 8,		//缓冲系数
+		direction: 1, //正方向-1,反方向-0
+		paused: false,
+		arrLink: []
+	},
+	slideFade: function() {		//淡入淡出模式
+		// if (this.defaults.type != 'fade') return false;
+		var num = 0;
+		var _this = this;
+		_this.timer = autoPlay();
+		function autoPlay() {
+			return setInterval(fade(_this.oUl), _this.options.timeout); 
+		};
+		function fade(obj) {
+			var len = obj.children.length;
+			function setOriginalState(index) {
+				for (var k = 0; k < len; k++) {
+					//设置定位和层级,透明度,过渡
+					setOpacity(obj.children[k], 0);
+					obj.children[k].style.cssText += 'transition: all ' + ( _this.options.average / 10 ) + 's;left: 0;top: 0;z-index: 1';
+				}
+				setOpacity(obj.children[index], 100);
+				obj.children[index].style.zIndex = index + len;
+			}
+			setOriginalState(0);
+			return function tabImg(){
+					_this.options.direction ? num++ : num-- ; 
+					if (num == len) {
+						num = 0;
+					}
+					if (num < 0) {
+						num = len - 1;
+					}
+					setOriginalState(num);
+				};
+				// if (_this.options.paused){
+				// 	clearInterval(obj.timer)
+				// } else {
+					// _this.timer = setInterval(tabImg, _this.options.timeout);
+				// }
+			};
+	},
+	slideMove: function() {		//滑动模式
+		// if (this.defaults.type != 'slide') return false;
+		
+		var num = 0;
+		var _this = this;
+		_this.timer = autoPlay();
+		function autoPlay() {
+			return setInterval(swipe(_this.oUl), _this.options.timeout); 
+		};
+		function swipe(obj) {
+			var timer = null;
+			var newFirst = obj.children[0].cloneNode(true);  //实现无缝隙，添加一张辅助图片
+			obj.appendChild(newFirst);
+			var aLi = document.getElementsByTagName('li');
+			var len = obj.children.length;
+			for (var i = 0; i < len; i++) {
+				//设置图片的定位
+				obj.children[i].style.left = (i % len) * obj.children[0].offsetWidth + 'px';
+			}
+			function autoPlay(){
+				    timer = setInterval(function(){
+				    move();
+				    },_this.options.timeout);
+			}
+			// if (newOptiongs.paused) {
+			// 	clearInterval(timer);
+			// } else {
+			// 	autoPlay(obj);
+			// }
+			// autoPlay();
+			return function move(){
+				_this.options.direction ? num++ : num-- ; 
+			    if(num == len){  //当图片到最后一张的时候，将oUl的left值设为0重新开始
+			         obj.style.left = 0;
+			        num = 1;
+			    } else if (num == -1){
+			        obj.style.left = (-(len-1)*obj.children[0].offsetWidth) + 'px';
+			        num = len-2;
+			    }
+				doMove(obj, -num*obj.children[0].offsetWidth, 'left', _this.options.average, 30);
+			}
+		}
+	},
+	paused: function() {
+		clearInterval(this.timer);
+		console.log(this);
+	},
+	play: function() {
+		// this.timer = this.timer;
+		console.log(this.timer);
+	}
+};
+var json = {
+	arrImg: ['img/1.jpg', 'img/2.jpg', 'img/3.jpg', 'img/4.jpg', 'img/5.jpg', 'img/6.jpg'],
+	arrLink: ['www.qq.com', 'www.taobao.com', 'www.jd.com', 'www.ienovo.com', 'www.imooc.com', 'www.sougou.com']
+}
+var slider1 = new FlexSlider('wrap1', json);
+var slider2 = new FlexSlider('wrap2', json);
+slider1.slideFade();
+slider2.slideMove();
+var oBtn = document.getElementById('btn');
+oBtn.onclick = function() {
+	if (this.isClick) {
+		slider.play();
+		// this.isClick = !this.isClick;
+	} else {
+		slider.paused();
+	}
+	this.isClick = !this.isClick;
+	
+};
+// console.log(FlexSlider.prototype.defaults.type[0]);
+// console.log(slide.options);
+
